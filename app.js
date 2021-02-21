89df344ecb730571c531b176520903017ff99755
SCREENSABER <61020218+SCREENSABER@users.noreply.github.com>
2021-02-21 02:25:16 -0600

    pushing file

---
+++ app.js
@@ -0,0 +1,12 @@
+//new Set of ([1,1,2,2,3,4]) returns [1,2,3]
+//[...new Set("referee")].join("") returns ['r','e','f']
+//Map(2) {Array(3) => true, Array(3) => false}
+function hasDuplicate(ar) {
+    set = new Set()
+    set = hasDuplicate()
+}
+function VowelCount(str) {
+    str.map((val) => {
+        console.log(VowelCount)
+    })
+}
